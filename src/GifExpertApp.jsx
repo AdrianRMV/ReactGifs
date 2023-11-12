4fd69b8a6eb1463ba1d7faa2e1082d2243e345de
AdrianRMV <adrianthelawx@gmail.com>
2023-11-12 12:56:48 -0700

    React Gifs

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { AddCategory, GifGrid } from './components';
+
+export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['Mario']);
+
+    const onAddCategory = (newCategory) => {
+        const arregloMinus = [];
+
+        // * Agrega a otro arreglo todas las categorias existentes pero en minusculas
+        categories.forEach((category) =>
+            arregloMinus.push(category.toLowerCase())
+        );
+
+        if (arregloMinus.includes(newCategory.toLowerCase())) return; // Si la categoria ya existe que no lo agregue
+
+        setCategories([newCategory, ...categories]);
+    };
+
+    return (
+        <>
+            <a
+                href="https://portfolio-react-blush-kappa.vercel.app/"
+                title="Porfolio"
+                target="_blank"
+                rel="noreferrer"
+                className="logoContainer"
+            >
+                <img src="/LogoAdrian.png" alt="Logo Adrian" />
+            </a>
+            <h1>Buscador de Gifs</h1>
+
+            <AddCategory onNewCategory={(value) => onAddCategory(value)} />
+
+            {categories.map((category) => (
+                <GifGrid key={category} category={category} />
+            ))}
+
+            <footer>
+                <h2 className="Footer-text">By: Adrian Ramirez</h2>
+            </footer>
+        </>
+    );
+};
